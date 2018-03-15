344de91b63704a323512032c51ea3d19d4cc9dac
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-03-15 11:06:56 +1100

    Added babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   entry: ['./src/index.js'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
