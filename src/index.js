3b5effd9182ad325e0d66cdfedc656ed97ba4e47
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-03-15 11:16:56 +1100

    Updated to add react-hot-loader and basic react element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-console.log('====================================');
-console.log('My Minimal React Webpack Babel Setup');
-console.log('====================================');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My minimal react webpack babel setup';
+
+ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
