caac9b8992595d8f437ab0d79e7fdac114e5f060
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-03-15 12:18:29 +1100

    Updated to have eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader';
+import React from "react";
+import ReactDOM from "react-dom";
+import { hot } from "react-hot-loader";
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = "My Minimal React Webpack Babel Setup";
 
-const App = ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
+const App = ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
 
 export default hot(module)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['eslint-loader'],
+        use: ['babel-loader', 'eslint-loader'],
       },
     ],
   },
