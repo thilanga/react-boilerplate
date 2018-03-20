d5dbd6e85969f1f0a01ff577a0a5f969334d39f1
Thilanga Pitigala <pittado@gmail.com>
2018-03-20 11:03:15 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { hot } from "react-hot-loader";
 
-const title = "My Minimal React Webpack Babel Setup";
+const title = "Simple React-Webpack-Babel-Setup";
 
 const App = ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
 
